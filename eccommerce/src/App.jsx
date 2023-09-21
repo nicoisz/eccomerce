3c6c9d0fe44fa5f68a7286b4406d3719e1e664d5
nicoisz <nicolasignacio.sz@gmail.com>
2023-09-21 16:45:27 -0300

    adding sass and style

--- eccommerce/src/App.jsx
+++ eccommerce/src/App.jsx
@@ -1,25 +1,44 @@
+import "./categories.styles.scss";
+
 const App = () => {
-  const subtitle = "Shop Now";
   const categories = [
     {
       id: 1,
-      title: "Hats",
-      subtitle: subtitle,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: "Jackets",
-      subtitle: subtitle,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
   return (
     <div className="categories-container">
-      {categories.map(({ title, subtitle }) => (
-        <div className="category-container">
-          {/*image*/}
+      {categories.map(({ title, id, imageUrl }) => (
+        <div className="category-container" key={id}>
+          <div
+            className="background-image"
+            style={{ backgroundImage: `url(${imageUrl})` }}
+          ></div>
           <div className="category-body-container">
             <h2>{title}</h2>
-            <p>{subtitle}</p>
+            <p>Shop Now</p>
           </div>
         </div>
       ))}
