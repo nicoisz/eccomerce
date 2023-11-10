be14d78f30164c3e9689c84618955a346b03e4aa
nicoisz <nicolasignacio.sz@gmail.com>
2023-11-09 23:12:41 -0300

    fetching data from firebase collection

--- eccommerce/src/context/product.context.jsx
+++ eccommerce/src/context/product.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 import SHOP_DATA from "../shop-data.js";
-import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+import { addCollectionAndDocuments, getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 import { useEffect } from "react";
 
 export const ProductsContext = createContext({
@@ -9,12 +9,22 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-  const value = { products };
+
   
   // useEffect(() => { //loading data for the first time, uncomment
   //   addCollectionAndDocuments('categories', SHOP_DATA)
   // }, [])
   
+  useEffect(() => {
+    console.log('ho')
+
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap)
+    }
+    getCategoriesMap();
+  }, []);
+  const value = { products };
   return (
     <ProductsContext.Provider value={value}>
       {children}
