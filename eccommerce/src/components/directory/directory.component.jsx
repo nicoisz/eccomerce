636b5ae8833c0b7069c60db4288ff198b47eda6d
nicoisz <nicolasignacio.sz@gmail.com>
2023-11-11 23:16:51 -0300

    fixing styles clashes

--- eccommerce/src/components/directory/directory.component.jsx
+++ eccommerce/src/components/directory/directory.component.jsx
@@ -1,11 +1,11 @@
-import CategoryItem from "../category-item/category-items.component";
+import DirectoryItem from "../directory-item/directory-items.component";
 import "../directory/directory.styles.scss";
 
 const Directory = ({ categories }) => {
   return (
     <div className="categories-container">
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
   );
